d0c681b249e6e74cad0c7ef138f93dfbf8961345
Saul12344 <90734770+Saul12344@users.noreply.github.com>
2022-03-07 21:08:04 -0800

    Create practica2.scala

---
+++ practices/practica2.scala
@@ -0,0 +1,27 @@
+// 1. Develop an algorithm in scale that calculates the radius of a circle
+
+
+
+
+
+
+
+2. Develop an algorithm in scala that tells me if a number is prime
+ 
+
+
+
+
+
+
+
+3. Given the variable var bird = "tweet", use string interpolation to print "I am writing a tweet"
+ 
+
+4. Given the variable var message = "Hello Luke I am your father!" uses slice to extract the
+    "Luke" sequence
+ 
+5. What is the difference between values ​​(val) and a variable (var) in scala?
+Val is something immutable I mean it is an assigned value while var is mutable and is easily modified.
+
+6. Given the tuple (2,4,5,1,2,3,3.1416,23) it returns the number 3.1416
